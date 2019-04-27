441c61106106b42028e0079da9ab2a0aee765961
dbedoyat <dbedoya@outlook.com>
2019-04-28 01:39:58 +0700

    update token

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import content from '../content/home.md';
+import Head from 'next/head'
 
 export default class Home extends Component {
     render() {
         let { html, attributes: { title, cats } } = content;
         return (
+
+            <head>
+            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            </head>
             <article>
                 <h1>{title}</h1>
                 <div dangerouslySetInnerHTML={{ __html: html }} />
