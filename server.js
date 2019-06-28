d57cab624f002f934e99da0c0500f8153d494874
processoft <44810264+processoft@users.noreply.github.com>
2019-06-28 15:30:33 -0500

    Change from static to deploy

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
 const express = require('express');
-const next = require('next');
-
-
-const port = parseInt(process.env.PORT, 10) || 8080;
 const dev = process.env.NODE_ENV !== 'production';
+const next = require('next');
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const server = express();
 
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    // Here we are handling our custom route, this
+    // now work for server side rendering
+    server.get('/portfolio/:slug', (req, res) => {
+      const nextJsPage = '/post';
+      const queryParams = { slug: req.params.slug };
+      app.render(req, res, nextJsPage, queryParams);
+    });
 
+    server.get('*', (req, res) => handle(req, res));
 
-(async () => {
-  await app.prepare();
-
-
-
-
-  server.get('/post/:slug', (req, res) => {
-    const queryParams = { slug: req.params.slug, apiRoute: 'post' };
-    app.render(req, res, '/post', queryParams);
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready http://localhost:3000 <');
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-
-  server.get('*', (req, res) => (
-    handle(req, res)
-  ));
-
-  
-
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Listening on http://0.0.0.0:${port}`);
-  });
-})();
