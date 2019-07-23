22d44e846ba1bb21860616e27edb4768a4de3022
dbedoyat <dbedoya@outlook.com>
2019-07-22 19:26:43 -0500

    Test PHP

--- components/Formulario.js
+++ components/Formulario.js
@@ -5,8 +5,15 @@ class Formulario extends React.Component {
     render() {
         return (
             <div>
-            <p>Contact with me here</p>
-            <p>Click</p>
+
+            <form action="../mail.php" method="POST">
+            <p>Name</p> <input type="text" name="name" />
+            <p>Email</p> <input type="text" name="email" />
+            <p>Phone</p> <input type="text" name="phone" />
+
+            <input type="submit" value="Send" /><input type="reset" value="Clear" />
+            </form>
+
             </div>
            
         );
