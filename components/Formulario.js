e19af04bf9deff4c8ccdabd4252f006193a868d4
dbedoyat <dbedoya@outlook.com>
2019-07-22 19:44:24 -0500

    add php info

--- components/Formulario.js
+++ components/Formulario.js
@@ -6,7 +6,7 @@ class Formulario extends React.Component {
         return (
             <div>
 
-            <form action="../mail.php" method="POST">
+            <form action="../api/mail.php" method="POST">
             <p>Name</p> <input type="text" name="name" />
             <p>Email</p> <input type="text" name="email" />
             <p>Phone</p> <input type="text" name="phone" />
